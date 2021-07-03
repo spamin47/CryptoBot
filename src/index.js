deff85ce60970bd4230547740b4a8af4fc3096bf
Andrew Lao <43099770+AndrewLao@users.noreply.github.com>
2021-07-03 14:18:39 -0700

    finished bot maybe

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ var r_pass = process.env.ROBBINHOOD_PASS;
 var username = process.env.TRADINGVIEW_USER;
 var password = process.env.TRADINGVIEW_PASS;
 
-var wallet = 10.00; //cash in USD
+var wallet = 100.00; //cash in USD
 var amountTrading = 10; //amount to trade with
 
 
@@ -69,6 +69,7 @@ function runBot(chartPage, brokerPage){
     var atr1x_Sell = true;
     var atr1dot5x_Sell = true;
     var atr2x_Sell = true;
+    var started_below_riskATR_Bottom_value = false;
     //take profit atr: variables used for saving atr's after receiving a buy signal
     var atr_1x_TP;
     var atr_1dot5x_TP
@@ -78,10 +79,9 @@ function runBot(chartPage, brokerPage){
     var atr1dot5x_percent = 0.50;
     var atr2x_percent = 0.60;
     var CRSI_percent = 0.70;
-  
 
     var botInterval = setInterval(async () => {
-      await page.mouse.move(650, 200);
+      await page.mouse.move(565, 200);
 
       //collect data
       var now = new Date();
@@ -94,30 +94,37 @@ function runBot(chartPage, brokerPage){
 
       if(curSec == 59){
         await page.waitForTimeout(700);
-        let [buyOrSell_Signal] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[10]/div');
-        let buyOrSell = 
+        const [buyOrSell_Signal] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[10]/div');
+        const buyOrSell = 
           String(await page.evaluate(name => name.innerText, buyOrSell_Signal)).length; //buy or sell 1.00 = buy, -1.00 = sell; Buy: 4, Sell: 5;
-        let [atr_take_profit_1x] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[5]/div');
-        let atr_1x = parseFloat(await page.evaluate(name => name.innerText, atr_take_profit_1x));
-        let [atr_take_profit_1dot5x] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[7]/div');
-        let atr_1dot5x = parseFloat(await page.evaluate(name => name.innerText, atr_take_profit_1dot5x));
-        let [atr_take_profit_2x] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[16]/div');
-        let atr_2x = parseFloat(await page.evaluate(name => name.innerText, atr_take_profit_2x));
-        let [closing_price] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[1]/div[1]/div[2]/div/div[5]/div[2]'); //closing price
-        let price = parseFloat(await page.evaluate(name => name.innerText, closing_price));
-        let [dynamic_overbought] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[3]/td[2]/div/div[1]/div/div[2]/div[2]/div[2]/div/div[5]/div');
-        let dynamicOverboughtValue = parseFloat(await page.evaluate(name => name.innerText, dynamic_overbought));
-        let [crsi] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[3]/td[2]/div/div[1]/div/div[2]/div[2]/div[2]/div/div[7]/div');
-        let CRSIValue = parseFloat(await page.evaluate(name => name.innerText, crsi));
-        let [riskATR_Top] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[2]/div');
-        let riskATR_Top_Value = parseFloat(await page.evaluate(name => name.innerText, riskATR_Top));
-        let [riskATR_Bottom] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[3]/div');
-        let riskATR_Bottom_Value = parseFloat(await page.evaluate(name => name.innerText, riskATR_Bottom));
-        let static_overbought = 70;
-
+        const [atr_take_profit_1x] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[5]/div');
+        const atr_1x = parseFloat(await page.evaluate(name => name.innerText, atr_take_profit_1x));
+        const [atr_take_profit_1dot5x] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[7]/div');
+        const atr_1dot5x = parseFloat(await page.evaluate(name => name.innerText, atr_take_profit_1dot5x));
+        const [atr_take_profit_2x] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[16]/div');
+        const atr_2x = parseFloat(await page.evaluate(name => name.innerText, atr_take_profit_2x));
+        const [closing_price] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[1]/div[1]/div[2]/div/div[5]/div[2]'); //closing price
+        const price = parseFloat(await page.evaluate(name => name.innerText, closing_price));
+        const [dynamic_overbought] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[3]/td[2]/div/div[1]/div/div[2]/div[2]/div[2]/div/div[5]/div');
+        const dynamicOverboughtValue = parseFloat(await page.evaluate(name => name.innerText, dynamic_overbought));
+        const [crsi] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[3]/td[2]/div/div[1]/div/div[2]/div[2]/div[2]/div/div[7]/div');
+        const CRSIValue = parseFloat(await page.evaluate(name => name.innerText, crsi));
+        const [riskATR_Top] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[2]/div');
+        const riskATR_Top_Value = parseFloat(await page.evaluate(name => name.innerText, riskATR_Top));
+        const [riskATR_Bottom] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[3]/div');
+        const riskATR_Bottom_Value = parseFloat(await page.evaluate(name => name.innerText, riskATR_Bottom));
+        const static_overbought = 70;
+        await page.keyboard.press('ArrowLeft');
+        const [prevBuyOrSell_Signal] = await page.$x('/html/body/div[2]/div[1]/div[2]/div[1]/div/table/tr[1]/td[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[10]/div');
+        const prevBuyOrSell_Signal_Value = 
+          String(await page.evaluate(name => name.innerText, prevBuyOrSell_Signal)).length;
+        await page.keyboard.press('ArrowRight');
+        
+        
         //compress data in JSON format
         var data = new Object();
         data.buyOrSell = buyOrSell;
+        data.prevBuyOrSell = prevBuyOrSell_Signal_Value;
         data.price = price;
         data.atrValue_1x = atr_1x;
         data.atrValue_1dot5x = atr_1dot5x;
@@ -133,7 +140,7 @@ function runBot(chartPage, brokerPage){
         console.log(`{data: ${dataJSON} }`);
 
         //buy
-        if(buyOrSell == 4 && !inTrade && !inTrade_ATR){ //if not any trade and given buy signal, buy
+        if((buyOrSell == 4 || prevBuyOrSell_Signal_Value == 4) && !inTrade && !inTrade_ATR){ //if not any trade and given buy signal, buy
           switchTab(page2);
           //unlocks take profit conditions
           inTrade = true;
@@ -153,7 +160,14 @@ function runBot(chartPage, brokerPage){
             inTrade = false;
             inTrade_ATR = true;
           }
-
+          //bought below riskATR bottom value
+          if(price <= riskATR_Bottom_Value){
+            started_below_riskATR_Bottom_value = true;
+          }
+          else{
+            started_below_riskATR_Bottom_value = false;
+          }
+          
           if(CRSIValue >= dynamicOverboughtValue && CRSIValue >= static_overbought){
             cRSI_Sell = false;
           }
@@ -174,10 +188,15 @@ function runBot(chartPage, brokerPage){
           switchTab(page2);
           let crsiSellSignal = (CRSIValue >= dynamicOverboughtValue && CRSIValue >= static_overbought);
 
-          if(buyOrSell == 5 || 
-            (crsiSellSignal && price >= atr_2x_TP) ||price <= riskATR_Bottom_Value){ //extreme sell signal: sell all
+          if(buyOrSell == 5 || prevBuyOrSell_Signal_Value === 5 ||
+            (crsiSellSignal && price >= atr_2x_TP)){ //extreme sell signal: sell all
             inTrade = false;
             console.log("sell all " + now.getHours() + ":" + curMin + ":" + curSec);
+            let sellingBoi = await sell(page2, 1);
+            console.log(`Sold: ${sellingBoi}`);
+          }else if(price <= riskATR_Bottom_Value && !started_below_riskATR_Bottom_value){
+            inTrade = false;
+            console.log("sell all at " + now.getHours() + ":" + curMin + ":" + curSec + ", reason: below riskATR");
             let sellingBoi = await sell(page2, 1);
             console.log(`Sold: ${sellingBoi}`);
           }else if(crsiSellSignal && cRSI_Sell){//very hard sell signal
@@ -192,6 +211,7 @@ function runBot(chartPage, brokerPage){
             console.log(`Sold: ${sellingBoi}`);
           }else if(price >= atr_1dot5x_TP && atr1dot5x_Sell){ //medium sell signal
             atr1dot5x_Sell = false;
+            atr1x_Sell = false;
             console.log("atr1.5x sell at " + now.getHours() + ":" + curMin + ":" + curSec);
             let sellingBoi = await sell(page2, atr1dot5x_percent);
             console.log(`Sold: ${sellingBoi}`);
@@ -213,7 +233,7 @@ function runBot(chartPage, brokerPage){
             console.log("sell all " + now.getHours() + ":" + curMin + ":" + curSec);
             let sellingBoi = await sell(page2,1);
             console.log(`Sold: ${sellingBoi}`);
-          }else if(buyOrSell == 5 || 
+          }if(buyOrSell == 5 || prevBuyOrSell_Signal_Value === 5 ||
             (crsiSellSignal && price >= atr_2x_TP) ||price <= riskATR_Bottom_Value){ //extreme sell signal: sell all
             inTrade_ATR = false;
             inTrade = false;
@@ -259,18 +279,7 @@ async function buy(oldPage, multiplier, tradingViewPrice){
     const page = oldPage;
     const thisAmount = wallet * multiplier;
 
-    const [robbinHood_price] = await page.$x('/html/body/div[1]/main/div[3]/div/div/div/div/div/div/div/div[1]/section[1]/header/div[1]/h1/span/span');
-    const robbinHoodPrice_Value = await page.evaluate(name => name.innerText, robbinHood_price);
-    let robbinHoodPrice_Value_Float = parseFloat(robbinHoodPrice_Value.substring(1,3) + robbinHoodPrice_Value.substring(4));
-    console.log("Robinhood's price: " + robbinHoodPrice_Value_Float);
-    let difference = await percentDiff(tradingViewPrice, robbinHoodPrice_Value_Float);
-    console.log(difference);
-    
-    if(difference >= 0.1){
-      return false;
-    }
-
-    console.log("buying: " + thisAmount);
+    console.log("Trying to buy: " + thisAmount);
     //click on "buy tab"
     await page.waitForTimeout(2000);
     const [button] = await page.$x("/html/body/div[1]/main/div[3]/div/div/div/div/div/div/div/div[2]/div/div[1]/div[2]/form/div[1]/header/div/div[1]/div/div[1]/div/h3/span/div");
@@ -280,8 +289,21 @@ async function buy(oldPage, multiplier, tradingViewPrice){
     await page.waitForTimeout(2000);
     await page.click('._2SOJcom0wr47t2LX78YQjj');//q9sjVKdRjdz3ZNAJdN0uj text box with price
     await page.keyboard.type(thisAmount.toString());
-    await page.waitForTimeout(500);
+    await page.waitForSelector('.css-1o0fjrg');
     await page.click('.css-1o0fjrg'); //review order
+
+    const [robbinHood_price] = await page.$x('/html/body/div[1]/main/div[3]/div/div/div/div/div/div/div/div[1]/section[1]/header/div[1]/h1/span/span');
+    const robbinHoodPrice_Value = await page.evaluate(name => name.innerText, robbinHood_price);
+    let robbinHoodPrice_Value_Float = parseFloat(robbinHoodPrice_Value.substring(1,3) + robbinHoodPrice_Value.substring(4));
+    console.log("Robinhood's price: " + robbinHoodPrice_Value_Float);
+    let difference = await percentDiff(tradingViewPrice, robbinHoodPrice_Value_Float);
+    console.log(difference);
+    
+    if(difference >= 0.1){
+      await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] }); //reload page
+      return false;
+    }
+  
     await page.click('.css-1o0fjrg'); //buy
     await page.waitForTimeout(3000);
     await page.click('.css-1o0fjrg'); //done
@@ -316,14 +338,14 @@ async function sell(oldPage, percentage){
     const [amount] = await page.$x('/html/body/div[1]/main/div[3]/div/div/div/div/div/div/div/div[2]/div/div[1]/div[2]/form/div[1]/footer');
     const amountAvailable_Str = await page.evaluate(name => name.innerText, amount);
     let amountAvailable = parseFloat(amountAvailable_Str.substring(1));
-    if(amountAvailable < 100){ //in case there's an error
+    if(amountAvailable < 200){ //in case there's an error
       amountAvailable *= multiplier;
       console.log("selling: " + amountAvailable);
       await page.click('._2SOJcom0wr47t2LX78YQjj');//_2SOJcom0wr47t2LX78YQjj text box 
       await page.keyboard.type(amountAvailable.toString());
-      await page.waitForTimeout(1000);
+      await page.waitForSelector('.css-1o0fjrg');
       await page.click('.css-1o0fjrg'); //review order
-      await page.waitForTimeout(500);
+      await page.waitForSelector('.css-1o0fjrg');
       await page.click('.css-1o0fjrg'); //sell
       await page.waitForSelector('.css-1o0fjrg');
       await page.click('.css-1o0fjrg');
